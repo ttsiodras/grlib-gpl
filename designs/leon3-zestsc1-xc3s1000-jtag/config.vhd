aa0caec31e4ab42fac1b07e3e48ca219c7a2f43d
Thanassis Tsiodras <ttsiodras@gmail.com>
2020-12-23 22:19:40 +0100

    Looks like this can go faster... Trying 37MHz.

--- designs/leon3-zestsc1-xc3s1000-jtag/config.vhd
+++ designs/leon3-zestsc1-xc3s1000-jtag/config.vhd
@@ -29,8 +29,8 @@ package config is
   -- my design can easily reach this speed.
   --
   -- 48*5/7 ~= 34
-  constant CFG_CLKMUL : integer := (5);
-  constant CFG_CLKDIV : integer := (7);
+  constant CFG_CLKMUL : integer := (7);
+  constant CFG_CLKDIV : integer := (9);
   constant CFG_OCLKDIV : integer := 1;
   constant CFG_OCLKBDIV : integer := 0;
   constant CFG_OCLKCDIV : integer := 0;
